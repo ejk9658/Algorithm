fdb06a0581f87db8d340e0e796aa04acad6bcab1
eunjin kang <55371726+ejk9658@users.noreply.github.com>
2022-07-13 14:42:45 +0900

    D2 : 2001. 파리 퇴치

--- src/swexpertacademy/Solution.java
+++ src/swexpertacademy/Solution.java
@@ -1,10 +1,38 @@
 package swexpertacademy;
+// D2 : 2001. 파리 퇴치
+
+import java.util.*;
 
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int sum_count;
+		int answer;
 
+		for(int tc=1; tc<=T; tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			int[][] arr = new int[N][N];
+			for(int i=0; i<N; i++)
+				for(int j=0; j<N; j++)
+					arr[i][j] = sc.nextInt();
+			
+			answer = 0;
+			for(int i=0; i<=N-M; i++)
+				for(int j=0; j<=N-M; j++) {
+					sum_count = 0;
+					for(int k=0; k<M; k++)
+						for(int l=0; l<M; l++)
+							sum_count += arr[i+k][j+l];
+					answer = Math.max(answer, sum_count);
+			}
+			
+			System.out.println("#"+tc+" "+answer);
+		}
 	}
 
 }
