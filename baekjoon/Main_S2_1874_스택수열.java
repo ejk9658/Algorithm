c3cc8007f66fe459ff80ea34339a9d5f9f0cd1ba
ejk9658 <ejk9658@naver.com>
2023-06-22 23:45:00 +0900

    Add: BOJ 1874 스택

---
+++ baekjoon/Main_S2_1874_스택수열.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_S2_1874_스택수열 {
+
+	static StringBuilder sb;
+	static int N, map[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		map = new int[N];
+		for(int i=0; i<N; i++) {
+			map[i] = Integer.parseInt(br.readLine());
+		} //read
+		
+		int idx = 0;
+		for(int i=1; i<=N; i++) {
+			
+		}
+		System.out.println(br.toString());
+	}
+}
